4f6c19cc3acfb7228c7000e16ac3b65e8614fc66
veenbreeze <mpambijevictor04@gmail.com>
2025-12-17 18:32:14 +0300

    changes in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,13 @@
-from rest_framework import serializers
+from rest_framework import serializers  
 from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.models import User 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
-
+  
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
